42d9c0204ba2b03a90208d3f08e15cc856e0270a
DESKTOP-M34TIR0\chltkdgnsska <tkdgns685@naver.com>
2023-08-07 21:43:17 +0800

    feat:코드 주석 처리

--- OnlineLibrary/OnlineLibrary/main.cpp
+++ OnlineLibrary/OnlineLibrary/main.cpp
@@ -8,7 +8,7 @@ int GetBookNameLength(char *bookName) // 문자열 처럼 쓰고 있다.
 	int len = 0;
 	while (true)
 	{
-		if (startPtr == NULL)
+		if (*startPtr == NULL)
 			break;
 
 		startPtr++;
@@ -20,8 +20,8 @@ int GetBookNameLength(char *bookName) // 문자열 처럼 쓰고 있다.
 int main()
 {
 	printf(">> 온라인 도서관 <<\n\n");
-	char* bookNameArr[100];
-	int bookIndex = 0;
+	char bookNameArr[100][20]; // 문자열 100개를 의미함.
+	int bookIndex = 0; // 문자열의 인덱스 : 각 책의 번호
 	while (true)
 	{
 		int number = 0;
@@ -29,30 +29,28 @@ int main()
 		scanf("%d", &number);
 		if (number == 1)
 		{
-			char bookName[20];
 			printf("\n>> 책 이름을 입력하세요 : ");
-			scanf("%s", bookName);
-			bookNameArr[bookIndex] = bookName;
+			scanf("%s", bookNameArr[bookIndex]);
 			bookIndex++;
 			printf("*******************************\n");
 		}
 		else if (number == 2)
 		{
-			char bookName[20];
-
+			char inputBookName[20];
 			printf("\n>> 찾을 책 이름을 작성하세요 : ");
-			scanf("%s", bookName);
+			scanf("%s", inputBookName);
 
 			for (int i = 0; i < bookIndex; i++)
 			{
-				char* storeBookName = bookNameArr[i];
+				char* storeBookName = bookNameArr[i]; // storeBookName : 책 이름.
 
-				int inputBookLen = GetBookNameLength(bookName);
-				int storeBookLen = GetBookNameLength(storeBookName);
+				int inputBookLen = GetBookNameLength(inputBookName); // => inputBookName(찾고자 하는 책 이름) : "abc" 
+				int storeBookLen = GetBookNameLength(storeBookName); // => storeBookName(저장된 책 이름) : "abc"
 
+				// 두 책 이름의 길이가 같은지 확인
 				if (inputBookLen == storeBookLen)
 				{
-					printf("책이 존재합니다\n");
+					// 각각의 문자열 비교해서 처리할 것.
 				}
 
 				// 문자열의 길이를 구하는 함수 구현		
